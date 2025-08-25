de579f73dc2827ce8b2408a569c052600ac65084
Chris-wars <bloodynova88@gmail.com>
2025-08-25 12:28:45 +0200

    feat(UI-103): Basis-App-Layout mit Header und Main-Bereich implementiert

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,23 @@ function App() {
   
 
   return (
-    <>
-      <h1 className="text-5xl font-extrabold text-green-600 text-center p-8">Resource Catalog UI</h1>
-    </>
+    <div className="min-h-screen bg-gray-100 font-sans antialiased">
+      <header className="bg-gradient-to-r from-blue-600 to-indigo-700 p-4 shadow-lg">
+        <div className="container mx-auto flex justify-between items-center">
+          <h1 className="text-3xl font-extrabold text-white">
+            Resource Catalog UI
+          </h1>
+          <nav>{/* Hier Kommt die Navigation */}  </nav>
+        </div>
+      </header>
+
+      <main className="container mx-auto p-4 mt-4">
+        <p className="text-gray-700 text-lg">Willkommen im Resource Catalog UI! Hier finden Sie eine Übersicht über alle verfügbaren Ressourcen.</p>
+
+
+      </main>
+
+    </div>
   )
 }
 
