69977d19edef351df1792d89352c5d76c46ad2ba
Chris-wars <bloodynova88@gmail.com>
2025-08-28 11:41:25 +0200

    improv: Refactored date formatting by implementing a util with data checks.

---
+++ src/utils/formatDate.js
@@ -0,0 +1,15 @@
+export const formatDate = (value, locale = 'de-DE', 
+                                  options = {
+                                            year: 'numeric',
+                                            month: 'long',
+                                            day: 'numeric',
+                                            hour: '2-digit',
+                                            minute: '2-digit'
+
+                                }
+                                ) => {
+    if (!value) return 'N/A';
+    const date = new Date(value);
+    if (Number.isNaN(date.getTime())) return 'N/A';
+    return date.toLocaleDateString(locale, options);
+};
